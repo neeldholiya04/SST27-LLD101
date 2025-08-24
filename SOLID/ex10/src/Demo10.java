5d369d661ceef11f786ad9a980af3c7892cfd167
neeldholiya04 <neeldholiya04@gmail.com>
2025-08-24 21:02:18 +0530

    ex10 completed

--- SOLID/ex10/src/Demo10.java
+++ SOLID/ex10/src/Demo10.java
@@ -1,5 +1,14 @@
+/*
+ * Issues in existing implementation:
+ * ReportService is responsible for report generation but also decides how logging is done by instantiating ConsoleLogger. This violates the Single Responsibility Principle.
+ * If tomorrow you want a FileLogger or DatabaseLogger, you must modify ReportService. This violates the Open/Closed Principle.
+ * ReportService depends on ConsoleLogger (concrete), instead of an abstraction. This violates the Dependency Inversion Principle.
+ */
 public class Demo10 {
     public static void main(String[] args) {
-        new ReportService().generate();
+        Logger logger = new ConsoleLogger();
+        ReportService service = new ReportService(logger);
+
+        service.generate();
     }
 }
